b3ab54ea5687c342cf6c808b2502a35256dac64a
Jim Ryan <jimmih15@gmail.com>
2021-01-17 09:35:18 +0000

    Timer working

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,8 +1,25 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Timer = ({ userData }) => {
 	const [currentTime, setCurrentTime] = useState(new Date());
+	useEffect(() => {
+		setTimeout(() => setCurrentTime(new Date()), 1000);
+	}, [currentTime]);
+	let totalMillisecondDif,
+		seconds,
+		minutes,
+		hours,
+		days = 0;
+	if (userData.hasOwnProperty('currentFast')) {
+		totalMillisecondDif =
+			currentTime.getTime() - userData.currentFast.startTime.getTime();
+		seconds = Math.floor(totalMillisecondDif / 1000) % 60;
+		minutes = Math.floor(totalMillisecondDif / 60000) % 60;
+		hours = Math.floor(totalMillisecondDif / 3600000) % 24;
+		days = Math.floor(totalMillisecondDif / 86400000);
+	}
+
 	return (
 		<Container className='py-3'>
 			<Row>
@@ -13,13 +30,21 @@ const Timer = ({ userData }) => {
 				</Col>
 			</Row>
 			<Row>
-				<Col sm='2'>
+				<Col sm='4'>
 					{userData.hasOwnProperty('currentFast') ? (
-						<>
-							Millisec Dif:{' '}
-							{currentTime.getTime() - userData.currentFast.startTime.getTime()}
-							{console.log(typeof userData.currentFast.startTime)}
-						</>
+						<h2>
+							Total Millisec Dif:
+							{totalMillisecondDif}
+							<br />
+							Seconds: {seconds}
+							<br />
+							Minutes: {minutes}
+							<br />
+							Hours: {hours}
+							<br />
+							Days: {days}
+							<br />
+						</h2>
 					) : (
 						'You must start a fast to see the time'
 					)}
